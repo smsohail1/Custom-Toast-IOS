97086ba749f0b7ef00ec7147e69aceabaedd5c9d
Abdul Ahad <abdulahad@Abduls-iMac.local>
2017-07-31 04:35:55 -0700

    Version 1.0

--- UISearchController/ViewController.swift
+++ UISearchController/ViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UITextView!
+    struct  data {
+        var name = String()
+        var desc = String()
+    }
+    
+    var list = [data(name:"sohail",desc:"1st")];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //list = [data(name:"sohail",desc:"1st")]
+        list.append(data(name:"second",desc:"2nd"))
+        
+        for i in list{
+            print("\(i)")
+        }
+//label.text = String(list.count)
+ //       showToast(message: "first")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func showToast(message : String) {
+        
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 8.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
     }
-
-
 }
 
